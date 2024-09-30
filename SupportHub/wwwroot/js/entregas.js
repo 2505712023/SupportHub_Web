dffed87f539b3770d1bbb9918160edf806c21ec6
csura <2505712023@mail.utec.edu.sv>
2024-09-29 21:31:57 -0600

    Mejora de la barra para filtrar/buscar entregas

---
+++ SupportHub/wwwroot/js/entregas.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $("#busqueda").on("keyup", function () {
+        var filtroSeleccionado = $("#filtros").val();
+        var valorAFiltrar = $(this).val().toLowerCase();
+        console.log("Entra al evento keyup.")
+        $("#tablaEntregas tbody tr").filter(function () {
+            var textoColumna = $(this).find('td').eq(filtroSeleccionado).text().toLowerCase();
+
+            $(this).toggle(textoColumna.indexOf(valorAFiltrar) > -1);
+        });
+    });
+});
