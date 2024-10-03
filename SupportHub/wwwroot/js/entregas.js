a33f70e0226700961cef8c784ca20f7c6df34e36
csura-brilo <csura@briloerp.com>
2024-10-03 07:50:32 -0600

    Adición de controles para agregar entregas en modalEntregas

--- SupportHub/wwwroot/js/entregas.js
+++ SupportHub/wwwroot/js/entregas.js
@@ -24,13 +24,57 @@
 });
 
 function openModal(opcion, button = null) {
+    // Guardamos los selects del modal
+    var selectIdTipoEntrega = $("#idTipoEntrega").closest("select").prop("outerHTML");
+    var selectIdEmpleadoEntrega = $("#idEmpleadoEntrega").closest("select").prop("outerHTML");
+    var selectIdEmpleadoRecibe = $("#idEmpleadoRecibe").closest("select").prop("outerHTML");
+    var selectIdEquipo = $("#idEquipo").closest("select").prop("outerHTML");
+
+    //Eliminamos los selects del modal ya que los vamos a insertar nuevamente pero en diferente orden
+    $("#idTipoEntrega").closest("select").remove();
+    $("#idEmpleadoEntrega").closest("select").remove();
+    $("#idEmpleadoRecibe").closest("select").remove();
+    $("#idEquipo").closest("select").remove();
+
     if (button === null) {
         if (opcion === "agregar") {
             $("#modalActionButton").text("Agregar");
-            $("select #idTipoEntrega").toggle();
-            $("select #idEmpleadoEntrega").toggle();
-            $("select #idEmpleadoRecibe").toggle();
-            $("select #idEquipo").toggle();
+            $("#modalBody").html(`
+                <label class="col-sm-12 col-form-label">Tipo de entrega:</label>
+                <div class="col-sm-12">
+                    ${selectIdTipoEntrega}
+                </div>
+
+                <label class="col-sm-12 col-form-label">Equipo:</label>
+                <div class="col-sm-12">
+                    ${selectIdEquipo}
+                </div>
+
+                <label class="col-sm-12 col-form-label">Cantidad:</label>
+                <div class="col-sm-12">
+                    <input type="number" class="form-control" name="cantidadEntrega" placeholder="0" />
+                </div>
+
+                <label class="col-sm-12 col-form-label">Fecha:</label>
+                <div class="col-sm-12">
+                    <input type="date" class="form-control" name="fechaEntrega" />
+                </div>
+
+                <label class="col-sm-12 col-form-label">Empleado entrega:</label>
+                <div class="col-sm-12">
+                    ${selectIdEmpleadoEntrega}
+                </div>
+
+                <label class="col-sm-12 col-form-label">Empleado recibe:</label>
+                <div class="col-sm-12">
+                    ${selectIdEmpleadoRecibe}
+                </div>
+
+                <label class="col-sm-12 col-form-label">Observaciones:</label>
+                <div class="col-sm-12">
+                    <textarea class="form-control" name="observacionEntrega" placeholder="Agregar observación (opcional)..."></textarea>
+                </div>
+            `);
         }
     }
 }
