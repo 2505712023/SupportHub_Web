4baa648a86964a7e7efce1489a1a299cdff312ec
GersonGit1 <ga5715303@gmail.com>
2024-10-03 16:56:19 -0600

    añadir una pagina vacía y crear un pequeño menú en la barra de navegación para acceder a ella simulando la versión desktop

---
+++ SupportHub/Pages/Usuario/mi_informacion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SupportHub.Pages.Usuario
+{
+    public class mi_informacionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
